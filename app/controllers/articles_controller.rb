dd70e9a7044ad25f181a43ce07c735c760e8b2d8
Kim <kim18_018@yahoo.com>
2017-08-06 21:24:50 +0800

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+  def index
+    @article = Article.all
+  end
+
+  def show
+    @article = Article.find( params[:id] )
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+    flash.notice = "New article created!"
+  end
+
+  def destroy
+    @article = Article.find( params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash.notice = @article.title + " Deleted"
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update( article_params )
+    
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+end
